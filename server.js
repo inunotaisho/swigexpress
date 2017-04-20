a452a1e7af78ce2d0b378d304aaf81aed3cf47e7
Ethan Fairweather <ebfairweather@gmail.com>
2017-04-20 17:32:37 -0400

    one more push

--- server.js
+++ server.js
@@ -10,13 +10,13 @@ app.use('/static', express.static('public'))
 
 app.get('/', (req, res) => res.render('index'));
 
-app.use(require('./lib/routes'));
-
 app.use('/register', require('./lib/routes/register'));
 
 app.get('/:template', (req, res) => {
   // This will look for '/views/${template}/index.html'
   res.render(`${req.params.template}`);
 })
+
+app.use(require('./lib/routes'));
 
 app.listen(1337, () => console.log('Listening...'));
